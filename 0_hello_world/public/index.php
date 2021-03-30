41041e633e129c13ee0a3bc0e18a52bc2a298666
Ivan Filho <ivanfilho21@gmail.com>
2021-03-30 17:17:39 -0300

    create hello world

---
+++ 0_hello_world/public/index.php
@@ -0,0 +1,69 @@
+<?php
+
+ini_set('display_errors', true);
+ini_set('display_startup_errors', true);
+error_reporting(E_ALL);
+
+// phpinfo();
+
+use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Loader;
+use Phalcon\Mvc\Application;
+use Phalcon\Mvc\Url as UrlProvider;
+use Phalcon\Mvc\View;
+
+define('BASE_PATH', dirname(__DIR__));
+define('APP_PATH', BASE_PATH . '/app');
+
+// registrar o autoloader
+$loader = new Loader();
+
+$loader->registerDirs(
+    [
+        APP_PATH . '/controllers',
+        APP_PATH . '/models/',
+    ]
+);
+
+$loader->register();
+
+
+// configurar as dependências
+
+$di = new FactoryDefault();
+
+$di['view'] = function() {
+    $view = new View();
+    $view->setViewsDir(APP_PATH . '/views/');
+    return $view;
+};
+
+
+// configurar URL principal
+$di['url'] = function() {
+    $url = new UrlProvider();
+    $url->setBaseUrl('/');
+    return $url;
+};
+
+// configurar conexão ao MySql
+$di['db'] = function() {
+    return new DbAdapter([
+        'host' => 'localhost',
+        'username' => 'ivanfilho',
+        'password' => 'root',
+        'dbname' => 'phalcon_db',
+    ]);
+};
+
+// tratamento da execução do App
+$app = new Application($di);
+
+try {
+    $res = $app->handle();
+    $res->send();
+    // echo $app->handle()->getContent();
+} catch(Exception $e) {
+    echo 'Erro na execução do App.<br>' .$e->getMessage() .'.';
+}
