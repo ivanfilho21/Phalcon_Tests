c686911f75d6d75a63dc14ce48a623ef5d27410a
Ivan Filho <ivanfilho21@gmail.com>
2021-03-31 16:04:06 -0300

    update

--- 1_signup_form/app/controllers/SignupController.php
+++ 1_signup_form/app/controllers/SignupController.php
@@ -7,4 +7,21 @@ class SignupController extends Controller {
     public function indexAction() {
         //
     }
+
+    public function registerAction() {
+        $user = new Users();
+
+        $res = $user->save(
+            $this->request->getPost(),
+            ['name', 'email']
+        );
+
+        if (!$res) {
+            $messages = $user->getMessages();
+    
+            foreach ($messages as $message) {
+                echo $message->getMessage(), '<br/>';
+            }
+        }
+    }
 }
