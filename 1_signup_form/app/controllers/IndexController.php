0b2280c06044a5814d8c2bd7d5f2177b5be6dcd8
Ivan Filho <ivanfilho21@gmail.com>
2021-03-31 15:55:05 -0300

    send a list of users to the view

--- 1_signup_form/app/controllers/IndexController.php
+++ 1_signup_form/app/controllers/IndexController.php
@@ -5,6 +5,7 @@ use Phalcon\Mvc\Controller;
 class IndexController extends Controller {
     
     public function indexAction() {
-        //
+        // sends the variable 'users' to the view
+        $this->view->users = Users::find();
     }
 }
