77dc547c0b1e656de00c2e7eff4efa1c214fb3af
Ivan Filho <ivanfilho21@gmail.com>
2021-04-18 11:41:53 -0300

    update

--- basic_template/app/controllers/IndexController.php
+++ basic_template/app/controllers/IndexController.php
@@ -4,12 +4,22 @@ class IndexController extends Phalcon\Mvc\Controller {
 
     function indexAction() {
         $this->view->disable();
+        $this->view->likes = Likes::count();
+
         echo $this->view->getRender('home', 'index');
     }
 
     function notFoundAction() {
         $this->view->disable();
         echo $this->view->getRender('layouts', '404');
+    }
+
+    function likeAction() {
+        $like = new Likes();
+        $like->setQuantity(1);
+        $like->create();
+
+        $this->response->redirect('');
     }
 
 }
