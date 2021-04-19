083b8b37108c26599cb9c2c9aff979a49743afe9
Ivan Filho <ivanfilho21@gmail.com>
2021-04-19 10:58:35 -0300

    create volt cache folder

--- invo/app/config/services.php
+++ invo/app/config/services.php
@@ -1,16 +1,33 @@
 <?php
 
-$di->set('view', function () {
-    global $config;
-
+$di->set('view', function () use ($config) {
     $view = new Phalcon\Mvc\View();
     $view->setViewsDir(APP_PATH . $config->application->viewsDir);
+    $view->registerEngines([
+        '.volt' => function ($view, $di) use ($config) {
+            $volt = new Phalcon\Mvc\View\Engine\Volt($view, $di);
+
+            $volt->setOptions(array(
+                'compiledPath' => APP_PATH . $config->application->voltCacheDir,
+                'compiledSeparator' => '_',
+                'compileAlways' => true,
+            ));
+
+            $compiler = $volt->getCompiler();
+            $compiler->addFunction('strftime', "strftime");
+            $compiler->addFunction('strtotime', "strtotime");
+            $compiler->addFunction('utf8_encode', "utf8_encode");
+            $compiler->addFunction('ucwords', "ucwords");
+
+            return $volt;
+        },
+        '.phtml' => Phalcon\Mvc\View\Engine\Php::class,
+    ]);
+
     return $view;
 });
 
-$di->set('url', function () {
-    global $config;
-
+$di->set('url', function () use ($config) {
     $url = new Phalcon\Mvc\Url();
     $url->setBaseUri($config->application->baseUri);
     return $url;
